4ba2aeb088702536198f5f13469b4cf0a5f9ef77
Fil_Maksim <filmaksim02@gmail.com>
2024-04-14 20:52:04 +0300

    Completed lab5

---
+++ lab5/src/main/kotlin/Queue.kt
@@ -0,0 +1,8 @@
+interface Queue<T : Any> {
+    fun enqueue(element: T): Boolean
+    fun dequeue(): T?
+    val count: Int
+    val isEmpty: Boolean
+        get() = count == 0
+    fun peek(): T?
+}
